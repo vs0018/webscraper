3b3d4480518ade0dbb4e615d9bbb4b1ec7bde0d0
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-03-26 23:27:22 -0400

    Add elements to scrape

---
+++ server.js
@@ -0,0 +1,37 @@
+var cheerio = require("cheerio");
+var axios = require("axios");
+
+// First, tell the console what server2.js is doing
+console.log("\n******************************************\n" +
+            "Grabbing every article headline and link\n" +
+            "from the HackerRank Blog:" +
+            "\n******************************************\n");
+
+// Making a request via axios for `nhl.com`'s homepage
+axios.get("https://blog.hackerrank.com/").then(function(response) {
+
+  // Load the body of the HTML into cheerio
+  var $ = cheerio.load(response.data);
+
+  // Empty array to save our scraped data
+  var results = [];
+
+  // With cheerio, find each li-tag and loop through the results
+  $("li").each(function(i, element) {
+
+    // Save the text of the a-tag as "title"
+    var title = $(element).find("a").text();
+
+    // Find the li tag's href value as "link"
+    var link = $(element).find("a").attr("href");
+
+    // Make an object with data we scraped for this h4 and push it to the results array
+    results.push({
+      title: title,
+      link: link
+    });
+  });
+
+  // After looping through each h4.headline-link, log the results
+  console.log(results);
+});
