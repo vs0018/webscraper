c77087c923cf2edd1a9472e1ad9dab3b39b14204
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-03-27 00:28:30 -0400

    fix mongodb URI

--- server.js
+++ server.js
@@ -27,7 +27,9 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/webscraper", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
 
 // Routes
 
