c2b95e8c9bc95ba599ad4a6707ba2124fa965d6f
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-03-28 19:11:21 -0400

    get note modal working and add style

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -113,7 +113,64 @@ app.get('/save/:id', function (req, res) {
     .then(function (result) {
       res.redirect('/')
     })
-    .catch(function (err) {
+    .catch(function(err) {
+      res.json(err)
+    });
+});
+
+//get route to retrieve all notes for a particlular article
+app.get("/getNotes/:id", function (req, res){
+  db.Article.findOne({_id: req.params.id})
+    .populate('notes')
+    .then(function(results) {
+      res.json(results)
+    })
+    .catch(function(err) {
+      res.json(err)
+    });
+});
+
+//get route to return a single note to view it
+app.get("/getSingleNote/:id", function (req, res) {
+  db.Note.findOne({_id: req.params.id})
+  .then(function(result) {
+    res.json(result)
+  })
+  .catch(function(err) {
+    res.json(err)
+  });
+});
+
+//post route to create a new note in the database
+app.post("/createNote", function (req, res){
+  let { title, body, articleId } = req.body;
+  let note = {
+    title,
+    body
+  };
+  db.Note.create(note)
+    .then(function(result) {
+      db.Article.findOneAndUpdate({_id: articleId}, {$push:{notes: result._id}}, {new:true})
+        .then(function(data) {
+          res.json(result)
+        })
+        .catch(function(err) {
+          res.json(err)
+        });
+    })
+    .catch(function(err) {
+      res.json(err)
+    });
+});
+
+//post route to delete a note
+app.post("/deleteNote", function(req, res) {
+  let {articleId, noteId} = req.body;
+  db.Note.remove({_id: noteId})
+    .then(function(result) {
+      res.json(result)
+    })
+    .catch(function(err) {
       res.json(err)
     });
 });
