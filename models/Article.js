da7033049eb550e41589db08da3a0686311241b2
Valerie S <43964677+vs0018@users.noreply.github.com>
2019-03-28 20:30:29 -0400

    get notes to persist and delete properly

--- models/Article.js
+++ models/Article.js
@@ -25,13 +25,12 @@ var ArticleSchema = new Schema({
     type: Boolean,
     default: false
   },
-  // `note` is an object that stores a Note id
-  // The ref property links the ObjectId to the Note model
-  // This allows us to populate the Article with an associated Note
-  note: {
-    type: Schema.Types.ObjectId,
-    ref: "Note"
-  }
+  notes: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: "Note"
+    }
+  ]
 });
 
 // This creates our model from the above schema, using mongoose's model method
